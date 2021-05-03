699e7e03cef28d1c862c970496e073063fc913f0
Alex Standiford <a@alexstandiford.com>
2021-05-03 16:24:02 -0700

    Fixes issues where extended plugins did not get registered items.

--- menus.php
+++ menus.php
@@ -4,13 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $class ) {
-	if ( 'Underpin\Underpin' === $class ) {
-		require_once( plugin_dir_path( __FILE__ ) . 'Menu.php' );
-		require_once( plugin_dir_path( __FILE__ ) . 'Menu_Instance.php' );
-		Underpin\underpin()->loaders()->add( 'menus', [
-			'instance' => 'Underpin_Menus\Abstracts\Menu',
-			'default'  => 'Underpin_Menus\Factories\Menu_Instance',
-		] );
-	}
+add_action( 'underpin/before_setup', function ( $instance ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'Menu.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'Menu_Instance.php' );
+	$instance->loaders()->add( 'menus', [
+		'instance' => 'Underpin_Menus\Abstracts\Menu',
+		'default'  => 'Underpin_Menus\Factories\Menu_Instance',
+	] );
 } );
