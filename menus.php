f208a804084cfc3682b801b70f1edf8145bf5d93
Alex Standiford <a@alexstandiford.com>
2021-05-05 17:16:15 -0700

    Corrects underpin autoloader

--- menus.php
+++ menus.php
@@ -4,11 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $file ) {
+add_action( 'underpin/before_setup', function ( $file, $class ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'Menu.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'Menu_Instance.php' );
-	Underpin\underpin()->get( $file )->loaders()->add( 'menus', [
+	Underpin\underpin()->get( $file, $class )->loaders()->add( 'menus', [
 		'instance' => 'Underpin_Menus\Abstracts\Menu',
 		'default'  => 'Underpin_Menus\Factories\Menu_Instance',
 	] );
-} );
+}, 10, 2 );
